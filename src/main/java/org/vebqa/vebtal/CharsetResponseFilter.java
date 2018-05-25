52d5971c2f1e27f60119e3a078a2c3c97cdc8904
kdoerges <kdoerges@cycronic.de>
2018-05-25 14:01:48 +0200

    new register for resources in plugin style

--- src/main/java/org/vebqa/vebtal/CharsetResponseFilter.java
+++ src/main/java/org/vebqa/vebtal/CharsetResponseFilter.java
@@ -1,32 +1,84 @@
 package org.vebqa.vebtal;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.Iterator;
+import java.util.List;
 
 import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
 import javax.ws.rs.core.MediaType;
 
+import org.glassfish.jersey.message.internal.ReaderWriter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class CharsetResponseFilter implements ContainerResponseFilter {
+public class CharsetResponseFilter implements ContainerRequestFilter, ContainerResponseFilter {
 
 	private static final Logger logger = LoggerFactory.getLogger(CharsetResponseFilter.class);
-	
+
 	public CharsetResponseFilter() {
 		logger.info("init CharsetResponseFilter");
 	}
-	
+
+	@Override
 	public void filter(ContainerRequestContext request, ContainerResponseContext response) throws IOException {
-		
-        MediaType contentType = response.getMediaType();
-        request.getHeaders().clear();
-        request.getHeaders().putSingle("Content-Type", contentType.toString() + ";charset=UTF-8");
-        response.getHeaders().clear();
-        response.getHeaders().putSingle("Content-Type", contentType.toString() + ";charset=UTF-8");
-        logger.info("extend header with utf-8");
+		MediaType contentType = response.getMediaType();
+		request.getHeaders().clear();
+		request.getHeaders().putSingle("Content-Type", contentType.toString() + ";charset=UTF-8");
+		response.getHeaders().clear();
+		response.getHeaders().putSingle("Content-Type", contentType.toString() + ";charset=UTF-8");
+		logger.info("extend header with utf-8");
 	}
 
+	@Override
+	public void filter(ContainerRequestContext request) throws IOException {
+		logger.info("Entering in Resource : /{} ", request.getUriInfo().getPath());
+		logQueryParameters(request);
+		
+        //log entity stream...
+        String entity = readEntityStream(request);
+        if(null != entity && entity.trim().length() > 0) {
+            logger.info("Entity Stream : {}",entity);
+        }
 
+	}
+
+	private void logQueryParameters(ContainerRequestContext requestContext) {
+        Iterator<String> iterator = requestContext.getUriInfo().getPathParameters().keySet().iterator();
+        while (iterator.hasNext()) {
+            String name = iterator.next();
+            List<String> obj = requestContext.getUriInfo().getPathParameters().get(name);
+            String value = null;
+            if(null != obj && obj.size() > 0) {
+                value = obj.get(0);
+            }
+            logger.info("Query Parameter Name: {}, Value :{}", name, value);
+        }
+    }
+	
+    private String readEntityStream(ContainerRequestContext requestContext)
+    {
+        ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+        final InputStream inputStream = requestContext.getEntityStream();
+        final StringBuilder builder = new StringBuilder();
+        try
+        {
+            ReaderWriter.writeTo(inputStream, outStream);
+            byte[] requestEntity = outStream.toByteArray();
+            if (requestEntity.length == 0) {
+                builder.append("");
+            } else {
+                builder.append(new String(requestEntity));
+            }
+            requestContext.setEntityStream(new ByteArrayInputStream(requestEntity) );
+        } catch (IOException ex) {
+            logger.info("----Exception occurred while reading entity stream :{}",ex.getMessage());
+        }
+        return builder.toString();
+    }
 }
--- src/main/java/org/vebqa/vebtal/RestServer.java
+++ src/main/java/org/vebqa/vebtal/RestServer.java
@@ -1,16 +1,27 @@
 package org.vebqa.vebtal;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.nio.charset.Charset;
 import java.util.Iterator;
 import java.util.ServiceLoader;
 
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
+
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
+import org.glassfish.jersey.process.Inflector;
 import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.server.model.Resource;
+import org.glassfish.jersey.server.model.ResourceMethod;
 import org.glassfish.jersey.servlet.ServletContainer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.vebqa.vebtal.model.Response;
 
 /**
  * Selenese REST API Service
@@ -30,8 +41,10 @@ public class RestServer {
 		// Plugins (Adapter) laden und ausfuehren
 		Iterator<TestAdaptionPlugin> plugins = ServiceLoader.load(TestAdaptionPlugin.class).iterator();
 		while (plugins.hasNext()) {
-			TestAdaptionPlugin robo = plugins.next();
-			if (robo.getType() == TestAdaptionType.ADAPTER) {
+			final TestAdaptionPlugin robo = plugins.next();
+			// register old-style adapter
+			if ((robo.getType() == TestAdaptionType.ADAPTER) && (robo.getImplementation() != null)) {
+				logger.info("register old style: " + robo.getName());
 				try {
 					config.register(robo.getImplementation());
 				} catch (Exception e) {
@@ -39,8 +52,68 @@ public class RestServer {
 				} catch (NoSuchMethodError e) {
 					logger.error("Error while starting plugin: " + robo.getName(), e);
 				}
+			} else if ((robo.getType() == TestAdaptionType.ADAPTER) && (robo.getImplementation() == null)) {
+				logger.info("register new style: " + robo.getName());
+				// register new-style adapter
+				final Resource.Builder resourceBuilder = Resource.builder();
+		        // path is always the adaptionID
+				resourceBuilder.path(robo.getAdaptionID());
+		        
+				// method is always "post"
+		        final ResourceMethod.Builder methodBuilder = resourceBuilder.addMethod("POST");
+		        
+		        methodBuilder.consumes(MediaType.APPLICATION_JSON).produces(MediaType.APPLICATION_JSON).handledBy(new Inflector<ContainerRequestContext, Response>() {
+		        	@Override
+		        	public Response apply(ContainerRequestContext containerRequestContext) {
+		        		// we will call the specifiy resource, build with the adaptionId
+		        		String tClassname = robo.getAdaptionID().toLowerCase().trim();
+		        		// erster Buchstabe gross
+		        		String tFirst = tClassname.substring(0, 1).toUpperCase(); 
+		        		String tRest = tClassname.substring(1);
+		        		tClassname = tFirst + tRest;
+		        		String tClass = "org.vebqa.vebtal."+robo.getAdaptionID() + "restserver." + tClassname + "Resource";
+		        		Response result = null;
+		        		
+		        		try {
+		        			Class<?> cmdClass = Class.forName(tClass);
+		        			Constructor<?> cons = cmdClass.getConstructor();
+		        			Object cmdObj = cons.newInstance();
+		        			
+		        			Class[] argTypes = new Class[] { String.class, String.class, String.class };
+		        			Method m = cmdClass.getDeclaredMethod("execute", argTypes);
+		        			
+		        			MultivaluedMap<String, String> pathparam = containerRequestContext.getUriInfo().getPathParameters();
+		        			
+		        			result = (Response)m.invoke(cmdObj); //, pathparam.getFirst("cmd"), pathparam.getFirst("target"), pathparam.getFirst("value"));
+		        			
+		        		} catch (ClassNotFoundException e) {
+		        			logger.error("Keyword class not found.", e);
+		        		} catch (NoSuchMethodException e) {
+		        			logger.error("Method not found!", e);
+						} catch (SecurityException e) {
+							logger.error("Security exception!", e);
+						} catch (InstantiationException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						} catch (IllegalAccessException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						} catch (IllegalArgumentException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						} catch (InvocationTargetException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+		        		
+		        		return result;
+		        	}
+				});
+		        
+		        final Resource resource = resourceBuilder.build();
+		        config.register(resource);
 			}
-		}
+		} // register adaption plugins
 
 		config.register(org.glassfish.jersey.moxy.json.MoxyJsonFeature.class);
 		config.register(CharsetResponseFilter.class);
