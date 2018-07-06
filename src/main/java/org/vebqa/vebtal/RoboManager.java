b43d51dd1535553b1d7035d94b97575b4d4ca297
kdoerges <kdoerges@cycronic.de>
2018-07-06 14:15:17 +0200

    rewrite logging

--- src/main/java/org/vebqa/vebtal/RoboManager.java
+++ src/main/java/org/vebqa/vebtal/RoboManager.java
@@ -3,78 +3,97 @@ package org.vebqa.vebtal;
 import java.util.Iterator;
 import java.util.ServiceLoader;
 
-import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.vebqa.vebtal.splash.AppPreloader;
 
+import com.sun.javafx.application.LauncherImpl;
+
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.encoder.PatternLayoutEncoder;
+import ch.qos.logback.classic.net.SocketAppender;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
-import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 @SuppressWarnings("restriction")
 public class RoboManager extends Application {
 
-	private static final Logger logger = LoggerFactory.getLogger(RoboManager.class);
-
+	public static final org.slf4j.Logger logger = LoggerFactory.getLogger(RoboManager.class);
+	
 	public static final RestServer singleServer = new RestServer();
 
+    public RoboManager() {
+    	// standard constructor
+	}
+    
 	public static void main(String[] args) {
-		launch(args);
+		LauncherImpl.launchApplication(RoboManager.class, AppPreloader.class, args);
 	}
 
-	@Override
-	public void start(Stage primaryStage) throws Exception {
-
-		// application title
-		// TODO: I18N
-		primaryStage.setTitle("Test Adaption Manager");
-
-		// BorderPane zur Aufnahme der Tabs
-		BorderPane mainPane = new BorderPane();
-		GuiManager.getinstance().getMain().setTabClosingPolicy(TabPane.TabClosingPolicy.UNAVAILABLE);
+    @Override
+    public void init() throws Exception {
+    	
+//    	LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
+//        PatternLayoutEncoder ple = new PatternLayoutEncoder();
+//
+//        ple.setPattern("%date %level [%thread] %logger{10} [%file:%line] %msg%n");
+//        ple.setContext(lc);
+//        ple.start();
+//        
+//        SocketAppender socketAppender = new SocketAppender();
+//    	socketAppender.setPort(4445);
+//    	socketAppender.setName("vebtal-manager");
+//    	socketAppender.setRemoteHost("127.0.0.1");
+//        
+//
+//    	Logger logger = (Logger) LoggerFactory.getLogger(RoboManager.class);
+//        logger.addAppender(socketAppender);
+//        logger.setLevel(Level.DEBUG);
+        
+        // BorderPane zur Aufnahme der Tabs
+		// BorderPane mainPane = new BorderPane();
+		GuiManager.getinstance().getMainTab().setTabClosingPolicy(TabPane.TabClosingPolicy.UNAVAILABLE);
 
 		// Create help tab
 		Tab tabHelp = new Tab();
 		// TODO: I18N
 		tabHelp.setText("Help");
-		GuiManager.getinstance().getMain().getTabs().add(tabHelp);
+		GuiManager.getinstance().getMainTab().getTabs().add(tabHelp);
 
-		mainPane.setCenter(GuiManager.getinstance().getMain());
+		GuiManager.getinstance().getMain().setCenter(GuiManager.getinstance().getMainTab());
 
 		// Log Area
 		/** Logs **/
-		mainPane.setBottom(GuiManager.getinstance().getLogArea());
-
-		// in stage einfuegen
-		primaryStage.setScene(new Scene(mainPane, 1024, 800));
-
+		GuiManager.getinstance().getMain().setBottom(GuiManager.getinstance().getLogArea());
+		
 		// Plugins laden und ausfuehren
 		Iterator<TestAdaptionPlugin> plugins = ServiceLoader.load(TestAdaptionPlugin.class).iterator();
 		if (!plugins.hasNext()) {
 			GuiManager.getinstance().writeLog("No plugins found!");
 		}
+		
+		double p = 10;
 		while (plugins.hasNext()) {
+			p = p + 10;
 			TestAdaptionPlugin robo = plugins.next();
-			GuiManager.getinstance().writeLog("Plugin of type (" + robo.getType() + ") found: " + robo.getName());
-
+			LauncherImpl.notifyPreloader(this, new AppPreloader.ActualTaskNotification("Start plugin of type (" + robo.getType() + "): " + robo.getName()));
 			// we will start adapter only at this point
 			if (robo.getType() == TestAdaptionType.ADAPTER) {
 				try {
-					GuiManager.getinstance().getMain().getTabs().add(robo.startup());
+					logger.info("Start plugin of type (" + robo.getType() + "): " + robo.getName());
+					GuiManager.getinstance().getMainTab().getTabs().add(robo.startup());
 				} catch (Exception e) {
 					logger.error("Error while starting plugin: " + robo.getName(), e);
 				}
 			}
+			Thread.sleep(1000);
 		}
-
-		// Close all -> shutdown all plugins
-		primaryStage.setOnCloseRequest(evt -> {
-			// execute own shutdown procedure
-			shutdown(primaryStage);
-		});
-
+		
+		// Start REST Server
 		Thread t = new Thread(new Runnable() {
 
 			public void run() {
@@ -84,9 +103,26 @@ public class RoboManager extends Application {
 
 		t.start();
 
+    }
+    
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		
+		// application title
+		// TODO: I18N
+		primaryStage.setTitle("Test Adaption Manager");
+
+		// in stage einfuegen
+		primaryStage.setScene(new Scene(GuiManager.getinstance().getMain(), 1024, 800));
+
+		// Close all -> shutdown all plugins
+		primaryStage.setOnCloseRequest(evt -> {
+			// execute own shutdown procedure
+			shutdown(primaryStage);
+		});
+
 		// anzeigen
 		primaryStage.show();
-
 	}
 
 	/**
@@ -102,14 +138,14 @@ public class RoboManager extends Application {
 			try {
 				robo.shutdown();
 			} catch (Exception e) {
-				logger.error("Error while starting plugin: " + robo.getName(), e);
+				// logger.error("Error while starting plugin: " + robo.getName(), e);
 			}
 		}
 		RoboManager.singleServer.shutdownServer();
 		try {
 			stop();
 		} catch (Exception e) {
-			logger.error("Error while stopping application.", e);
+			// logger.error("Error while stopping application.", e);
 		}
 		
 		// close this...
@@ -117,6 +153,6 @@ public class RoboManager extends Application {
 	}
 
 	public static void addTab(Tab aTab) {
-		GuiManager.getinstance().getMain().getTabs().add(aTab);
+		GuiManager.getinstance().getMainTab().getTabs().add(aTab);
 	}
 }
---
+++ src/main/java/org/vebqa/vebtal/splash/AppPreloader.java
@@ -0,0 +1,121 @@
+package org.vebqa.vebtal.splash;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javafx.application.Platform;
+import javafx.application.Preloader;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.TextAlignment;
+import javafx.stage.Stage;
+
+@SuppressWarnings("restriction")
+public class AppPreloader extends Preloader {
+	
+	private static final Logger logger = LoggerFactory.getLogger(AppPreloader.class);
+	
+    private static final double WIDTH = 400;
+    private static final double HEIGHT = 400;
+
+    private Stage preloaderStage;
+    private Scene scene;
+
+    private Label progress;
+
+    public AppPreloader() {
+    }
+
+    @Override
+    public void init() throws Exception {
+
+        Platform.runLater(() -> {
+            Label title = new Label("Starting VEBTAL Manager!\nLoading, please wait...");
+            title.setTextAlignment(TextAlignment.CENTER);
+            progress = new Label("0%");
+
+            VBox root = new VBox(title, progress);
+            root.setAlignment(Pos.CENTER);
+
+            scene = new Scene(root, WIDTH, HEIGHT);
+        });
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        this.preloaderStage = primaryStage;
+
+        // Set preloader scene and show stage.
+        preloaderStage.setScene(scene);
+        preloaderStage.show();
+    }
+
+    @Override
+    public void handleApplicationNotification(PreloaderNotification info) {
+        // Handle application notification in this point (see MyApplication#init).
+        if (info instanceof ProgressNotification) {
+            progress.setText(((ProgressNotification) info).getProgress() + "%");
+        }
+    }
+
+    @Override
+    public void handleStateChangeNotification(StateChangeNotification info) {
+        // Handle state change notifications.
+        StateChangeNotification.Type type = info.getType();
+        switch (type) {
+            case BEFORE_LOAD:
+                // Called after MyPreloader#start is called.
+                logger.debug("BEFORE_LOAD");
+                break;
+            case BEFORE_INIT:
+                // Called before MyApplication#init is called.
+                logger.debug("BEFORE_INIT");
+                break;
+            case BEFORE_START:
+                // Called after MyApplication#init and before MyApplication#start is called.
+                logger.debug("BEFORE_START");
+
+                preloaderStage.hide();
+                break;
+        }
+    }
+    
+    public static class ActualTaskNotification implements PreloaderNotification {
+
+    	private String details = "";
+
+        /**
+         * Constructs an error notification.
+         *
+         * @param details a string describing the error; must be non-null
+         */
+        public ActualTaskNotification(String details) {
+            if (details == null) throw new NullPointerException();
+
+            this.details = details;
+        }
+
+        /**
+         * Retrieves the description of the error.
+         * It may be the empty string, but is always non-null.
+         *
+         * @return the description of the error
+         */
+        public String getDetails() {
+            return details;
+        }
+
+        /**
+         * Returns a string representation of this {@code ErrorNotification} object.
+         * @return a string representation of this {@code ErrorNotification} object.
+         */
+        @Override public String toString() {
+            StringBuilder str = new StringBuilder("Preloader.ActualTaskNotification: ");
+            str.append(details);
+            return str.toString();
+        }
+    }    
+}
